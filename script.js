2979d2ddc9652d0790157a3780c756627ad1bf00
Mikita Maiseichek <Doktornikita@hotmail.com>
2019-10-28 16:31:14 +0300

    feat: add files project

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let panelElem = document.querySelector('.panel');
-console.log(panelElem);
 let titleElem = panelElem.querySelector('.panel--title');
-console.log(titleElem);
 titleElem.onclick = function () {
-    console.log('work');
     panelElem.classList.toggle('open');
 };
 //****************** SLIDER *******************//
