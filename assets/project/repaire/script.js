be674ac2d9eb62e65326e5bd6de54981532a6386
Mikita Maiseichek <Doktornikita@hotmail.com>
2019-10-29 01:47:28 +0300

    feat: finish JS

---
+++ assets/project/repaire/script.js
@@ -0,0 +1,19 @@
+const buttonMobile = document.querySelector('.controls--mobile');
+const buttonBack = document.querySelector('.controls--back');
+const frame = document.querySelector('.frame');
+const body = document.getElementsByTagName('body');
+let vwidth = document.documentElement.clientWidth;
+
+if (vwidth < 400) {
+    buttonMobile.style.display = 'none';
+}
+buttonMobile.addEventListener('click', function () {
+    if (buttonMobile.textContent === 'mobile') {
+        buttonMobile.textContent = 'desktop';
+        frame.style.width = '392px';
+    } else {
+        buttonMobile.textContent = 'mobile';
+        frame.style.width = '100%';
+    }
+});
+buttonBack.addEventListener('click', () => location.href = '../../../index.html');
